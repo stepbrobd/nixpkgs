76b40c3cc5c5e24bb686ab5f8f20aa8a3249823a
James Eapen <james.eapen@vai.org>
2026-02-20 12:51:40 -0500

    rPackages.iscream: use pkgs.htslib without libdeflate override
    now that pkgs.htslib is built with libdeflate support

--- pkgs/development/r-modules/default.nix
+++ pkgs/development/r-modules/default.nix
@@ -3136,19 +3136,13 @@ let
       '';
     });
 
-    iscream =
-      let
-        # https://huishenlab.github.io/iscream/articles/htslib.html
-        htslib-deflate = pkgs.htslib.overrideAttrs (attrs: {
-          buildInputs = attrs.buildInputs ++ [ pkgs.libdeflate ];
-        });
-      in
-      old.iscream.overrideAttrs (attrs: {
-        # Rhtslib (in LinkingTo) is not needed if we provide a proper htslib
-        propagatedBuildInputs =
-          builtins.filter (el: el != pkgs.rPackages.Rhtslib) attrs.propagatedBuildInputs
-          ++ [ htslib-deflate ];
-      });
+    iscream = old.iscream.overrideAttrs (attrs: {
+      # https://huishenlab.github.io/iscream/articles/htslib.html
+      # Rhtslib (in LinkingTo) is not needed if we provide a proper htslib
+      propagatedBuildInputs =
+        builtins.filter (el: el != pkgs.rPackages.Rhtslib) attrs.propagatedBuildInputs
+        ++ [ pkgs.htslib ];
+    });
 
     torch = old.torch.overrideAttrs (attrs: {
       preConfigure = ''
